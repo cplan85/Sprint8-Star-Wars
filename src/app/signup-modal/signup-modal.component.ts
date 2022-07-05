2ecc3331dde94b5eaabd032bd9aaf2cac1fd96f5
cplan85 <carlos_planchart@live.com>
2022-07-05 18:51:58 +0200

    updated validation form

--- src/app/signup-modal/signup-modal.component.ts
+++ src/app/signup-modal/signup-modal.component.ts
@@ -11,23 +11,47 @@ export class SignupModalComponent implements OnInit {
   signupForm: FormGroup;
   constructor(private _builder: FormBuilder) {
     this.signupForm = this._builder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      showPassword: ['', Validators.required],
-      getNewsletter: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+$')]],
+      lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      showPassword: [false],
+      getNewsletter: [false],
     });
   }
 
   ngOnInit(): void {}
 
-  send(values: any) {
-    let firstName = this.signupForm.value['firstName'];
-    console.log(firstName);
+  get firstName() {
+    return this.signupForm.get('firstName');
+  }
+  get lastName() {
+    return this.signupForm.get('lastName');
+  }
+  get email() {
+    return this.signupForm.get('email');
+  }
+  get password() {
+    return this.signupForm.get('password');
+  }
 
+  send(values: any) {
+    if (this.signupForm.valid) {
+      //add this USER To localstorage
+    }
+    let firstName = this.signupForm.value['firstName'];
     let lastName = this.signupForm.value['lastName'];
-    console.log(lastName);
+
+    let email = this.signupForm.value['email'];
+    console.log(email);
+
+    let password = this.signupForm.value['password'];
+    console.log(password);
+
+    let showPassword = this.signupForm.value['showPassword'];
+    console.log(showPassword);
+    let getNewsletter = this.signupForm.value['getNewsletter'];
+    console.log(getNewsletter);
 
     //this.budgetService.addBudgetItem(budgetName, customerName);
   }
