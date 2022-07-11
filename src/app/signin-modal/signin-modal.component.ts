06fae90b993036a347e996e056a154ec59ae24dd
Carlos Planchart <carlos_planchart@live.com>
2022-07-11 13:23:18 +0200

    signin modal update

--- src/app/signin-modal/signin-modal.component.ts
+++ src/app/signin-modal/signin-modal.component.ts
@@ -4,6 +4,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../interfaces/user';
+import { Router } from '@angular/router';
+import { delay } from 'rxjs';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-signin-modal',
@@ -18,7 +21,9 @@ export class SigninModalComponent implements OnInit {
   constructor(
     private localStorageService: LocalStorageService,
     private _builder: FormBuilder,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private router: Router,
+    private usersService: UsersService
   ) {
     this.signinForm = this._builder.group({
       email: ['', [Validators.required, Validators.email]],
@@ -62,24 +67,30 @@ export class SigninModalComponent implements OnInit {
 
   send(value: any) {
     let email = this.signinForm.value['email'];
+    const closeButton = document.getElementById('closeModalButton');
     if (this.signinForm.valid && this.localstorageUsers.length > 0) {
-
       const emailMatch = this.localstorageUsers.find((user) => user.email === email);
       if(emailMatch != null) {
+        //this should then lead to next modal for login with just password. jsmith@gmail.com
         this.message =  `Welcome back ${emailMatch.firstName} ${emailMatch.lastName}.`
+        this.usersService.logIn();
+       // let dothis = setTimeout(function(){ 
+          if (closeButton != null) {
+            closeButton.click();
+           
+            email="";
+          }
+        // }, 1000)
+         this.router.navigateByUrl('/starships');
+
+       //  clearInterval(dothis);
+        
+        console.log("triggered");
       }
     }
-   
-
-    const closeButton = document.getElementById('closeModalButton');
+    
 
     const newModal = document.getElementById('modelData2');
-    if (closeButton != null) {
-      closeButton.click();
-    }
-
-    //this.triggerModal(newModal);
-
-    //console.log(emailMatch, 'email from send');
+  
   }
 }
