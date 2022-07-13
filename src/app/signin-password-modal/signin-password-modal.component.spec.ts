59c13b49cf6152f75967f3a31bed9f858ac9e8db
cplan85 <carlos_planchart@live.com>
2022-07-13 21:31:05 +0200

    sign in password component unit test update

--- src/app/signin-password-modal/signin-password-modal.component.spec.ts
+++ src/app/signin-password-modal/signin-password-modal.component.spec.ts
@@ -1,6 +1,9 @@
+import { UsersService } from './../services/users.service';
+import { Router } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SigninPasswordModalComponent } from './signin-password-modal.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 describe('SigninPasswordModalComponent', () => {
   let component: SigninPasswordModalComponent;
@@ -8,18 +11,48 @@ describe('SigninPasswordModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SigninPasswordModalComponent ]
-    })
-    .compileComponents();
+      declarations: [SigninPasswordModalComponent],
+      providers: [
+        {
+          provide: FormBuilder,
+          useValue: jasmine.createSpyObj('FormBuilder', ['group']),
+        },
+        {
+          provide: Router,
+          useValue: jasmine.createSpyObj('Router', ['navigateByUrl']),
+        },
+        {
+          provide: UsersService,
+          useValue: jasmine.createSpyObj('UsersService', ['redirectUrl']),
+        },
+        {
+          provide: FormGroup,
+          useValue: jasmine.createSpyObj('FormGroup', ['get']),
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SigninPasswordModalComponent);
-    component = fixture.componentInstance;
+    const router = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    //component = fixture.componentInstance;
+
+    component = new SigninPasswordModalComponent(
+      new FormBuilder(),
+      router,
+      new UsersService()
+    );
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain 1 field', () => {
+    component.signinpasswordForm.value['password'] = '';
+    expect(component.signinpasswordForm.contains('password')).toBeTruthy();
   });
 });
--- src/app/signin-password-modal/signin-password-modal.component.ts
+++ src/app/signin-password-modal/signin-password-modal.component.ts
@@ -1,6 +1,6 @@
 import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder , FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { User } from '../interfaces/user';
 import { Router } from '@angular/router';
@@ -9,17 +9,17 @@ import { Input } from '@angular/core';
 @Component({
   selector: 'app-signin-password-modal',
   templateUrl: './signin-password-modal.component.html',
-  styleUrls: ['./signin-password-modal.component.scss']
+  styleUrls: ['./signin-password-modal.component.scss'],
 })
 export class SigninPasswordModalComponent implements OnInit {
   signinpasswordForm: FormGroup;
   localstorageUsers: User[] = [];
   @Input() public currentUser: User = {
-    firstName: "",
-    email: "",
-    lastName: "",
-    password: "",
-    getNewsletter: false, 
+    firstName: '',
+    email: '',
+    lastName: '',
+    password: '',
+    getNewsletter: false,
   };
 
   message: string = this.currentUser.email;
@@ -31,17 +31,14 @@ export class SigninPasswordModalComponent implements OnInit {
     this.signinpasswordForm = this._builder.group({
       password: ['', [Validators.required]],
     });
-   }
-
-  ngOnInit(): void {
-   
   }
 
+  ngOnInit(): void {}
+
   modal = document.getElementById('loginpasswordModal');
-  
-  
+
   onOpen(event: any) {
-    console.log("event",event);
+    console.log('event', event);
   }
 
   get password() {
@@ -50,7 +47,7 @@ export class SigninPasswordModalComponent implements OnInit {
 
   login() {
     this.usersService.logIn().subscribe((res) => {
-      console.log("response from login", res)
+      console.log('response from login', res);
       if (this.usersService.isLoggedIn) {
         const redirect = this.usersService.redirectUrl
           ? this.router.parseUrl(this.usersService.redirectUrl)
@@ -67,21 +64,15 @@ export class SigninPasswordModalComponent implements OnInit {
     const closeButton = document.getElementById('closepasswordModalButton');
     const passwordModalButton = document.getElementById('passwordModalButton');
     if (this.signinpasswordForm.valid) {
-    
-      
       if (this.currentUser.password === password) {
-        
         this.login();
         this.signinpasswordForm.reset();
-       
+
         if (closeButton != null) {
           closeButton.click();
         }
         this.router.navigateByUrl('/starships');
-
       }
     }
-
   }
-
 }
